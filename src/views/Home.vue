8acd7a85143d78f204374f65854b60459c72bfa9
Renato Novaes <renatoalmeida49@gmail.com>
2022-03-07 22:21:05 -0300

    feat(home.vue): vue-draggable for tabs!

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,11 +2,17 @@
   <div class="home">
     <h1>Home page</h1>
 
-    <div class="tab">
+    <div class="tab" :key="render">
       <div class="tab__string">
         <span>e</span>
-
-        <span v-for="n in 40" :key="n" @click="changeContent">-</span>
+        <Draggable v-model="eString">
+          <span
+            v-for="(note, index) in eString"
+            @dblclick="changeContent('eString', index)"
+            :key="`${note}-${index}`"
+            >{{ note }}</span
+          >
+        </Draggable>
       </div>
       <div class="tab__string">
         <span>B</span>
@@ -38,13 +44,35 @@
 </template>
 
 <script>
+import Draggable from "vuedraggable";
+
 export default {
   name: "Home",
 
+  components: {
+    Draggable,
+  },
+
+  data() {
+    return {
+      render: 0,
+      eString: ["-", "-", "3", "-", "-", "-", "-", "-", "-", "-", "-"],
+    };
+  },
+
   methods: {
-    changeContent(event) {
+    changeContent(string, index) {
+      this[string][index] = prompt(this[string][index]);
+      // event.target.innerHTML = prompt(event.target.innerHTML) || "-";
+      this.forceRender();
+    },
+
+    forceRender() {
+      this.render++;
+    },
+
+    check(event) {
       console.log(event);
-      event.target.innerHTML = prompt(event.target.innerHTML) || "-";
     },
   },
 };
