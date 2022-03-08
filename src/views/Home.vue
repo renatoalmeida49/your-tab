53dc7e2ee0127b6b4e9a029af926aa2705cabb26
Renato Novaes <renatoalmeida49@gmail.com>
2022-03-07 21:50:36 -0300

    feat(home.vue): first version of tab

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,57 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>Home page</h1>
+
+    <div class="tab">
+      <div class="tab__string">
+        <span>e</span>
+
+        <span v-for="n in 40" :key="n" @click="changeContent">-</span>
+      </div>
+      <div class="tab__string">
+        <span>B</span>
+
+        <span v-for="n in 40" :key="n" @click="changeContent">-</span>
+      </div>
+      <div class="tab__string">
+        <span>G</span>
+
+        <span v-for="n in 40" :key="n" @click="changeContent">-</span>
+      </div>
+      <div class="tab__string">
+        <span>D</span>
+
+        <span v-for="n in 40" :key="n" @click="changeContent">-</span>
+      </div>
+      <div class="tab__string">
+        <span>A</span>
+
+        <span v-for="n in 40" :key="n" @click="changeContent">-</span>
+      </div>
+      <div class="tab__string">
+        <span>E</span>
+
+        <span v-for="n in 40" :key="n" @click="changeContent">-</span>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
   name: "Home",
-  components: {
-    HelloWorld,
+
+  methods: {
+    changeContent(event) {
+      console.log(event);
+      event.target.innerHTML = prompt(event.target.innerHTML) || "-";
+    },
   },
 };
 </script>
+
+<style lang="scss">
+.tab {
+  font-family: monospace;
+}
+</style>
