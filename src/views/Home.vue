d8a8e3b1a61eb11f7c063cec0fbb45ad4d1dd09c
Renato Novaes <renatoalmeida49@gmail.com>
2022-03-19 11:04:55 -0300

    feat(home.vue): swap between lists

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,14 +2,22 @@
   <div class="home">
     <h1>Home page</h1>
 
-    <button @click="download('test.txt')">Click</button>
+    <button @click="download('tab.txt')">Download</button>
 
     <div id="all-tabs" class="tab" :key="render">
       <template v-for="(string, indexString) in guitar">
         <div class="tab__string" :key="indexString">
           <span>{{ string.note }}|</span>
-          <Draggable v-model="string.string" group="tablatura">
+          <Draggable
+            :list="string.string"
+            :move="handleMoveItem"
+            @end="handleDragEndItem"
+            group="tablatura"
+            :data-list="indexString"
+          >
             <span
+              :data-list="indexString"
+              class="note"
               v-for="(note, index) in string.string"
               @dblclick="changeContent(indexString, index)"
               :key="`${note}-${index}`"
@@ -138,8 +146,51 @@ export default {
       this.render++;
     },
 
-    check(event) {
-      console.log(event);
+    handleDragEndItem() {
+      if (this.originalList === this.futureList) {
+        this.movingItem =
+          this.guitar[this.futureList].string[this.originalIndex];
+        this.futureItem = this.guitar[this.futureList].string[this.futureIndex];
+
+        if (this.movingItem && this.futureItem) {
+          let _list = Object.assign([], this.guitar[this.futureList].string);
+
+          _list[this.futureIndex] = this.movingItem;
+          _list[this.originalIndex] = this.futureItem;
+
+          this.guitar[this.futureList].string = _list;
+        }
+      } else {
+        this.movingItem =
+          this.guitar[this.originalList].string[this.originalIndex];
+        this.futureItem = this.guitar[this.futureList].string[this.futureIndex];
+
+        if (this.movingItem && this.futureItem) {
+          let _listFrom = Object.assign(
+            [],
+            this.guitar[this.originalList].string
+          );
+          let _listTo = Object.assign([], this.guitar[this.futureList].string);
+
+          _listTo[this.futureIndex] = this.movingItem;
+          _listFrom[this.originalIndex] = this.futureItem;
+
+          this.guitar[this.originalList].string = _listFrom;
+          this.guitar[this.futureList].string = _listTo;
+        }
+      }
+    },
+
+    handleMoveItem(event) {
+      const { index, futureIndex } = event.draggedContext;
+
+      this.originalIndex = index;
+      this.futureIndex = futureIndex;
+
+      this.originalList = event.from.getAttribute("data-list");
+      this.futureList = event.to.getAttribute("data-list");
+
+      return false;
     },
   },
 };
@@ -150,8 +201,12 @@ export default {
   font-family: monospace;
   font-size: 20px;
 
-  .tab__string {
+  &__string {
     display: flex;
+
+    span {
+      cursor: pointer;
+    }
   }
 }
 </style>
