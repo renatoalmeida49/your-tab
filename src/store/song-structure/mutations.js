dffecc8d2e633f8c7e66f8d9412c424856f69271
Renato Novaes <renatoalmeida49@gmail.com>
2022-06-20 10:22:52 -0300

    feat(home.vue): new text-field

--- src/store/song-structure/mutations.js
+++ src/store/song-structure/mutations.js
@@ -6,6 +6,10 @@ const mutations = {
   removeContent(state, payload) {
     state.songStructure.splice(payload, 1);
   },
+
+  addTextField(state, payload) {
+    state.songStructure.splice(payload.index, 0, payload.component);
+  },
 };
 
 export default mutations;
