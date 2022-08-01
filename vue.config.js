b167dad96ed808b6e6bba56af1c0c33deb8a21a5
Renato Novaes <renatoalmeida49@gmail.com>
2022-08-01 11:14:48 -0300

    feat(vue.config.js): pre render plugin

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,15 @@
+const path = require("path");
+const PrerenderSPAPlugin = require("prerender-spa-plugin");
+
 module.exports = {
+  configureWebpack: {
+    plugins: [
+      new PrerenderSPAPlugin({
+        staticDir: path.join(__dirname, "dist"),
+        routes: ["/"],
+      }),
+    ],
+  },
   css: {
     loaderOptions: {
       sass: {
